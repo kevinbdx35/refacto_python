66288deb8b4cc08430d2e1e12a06917d1a7eccd6
kevin <kevin@kevin.home>
2022-12-28 14:58:12 +0100

    update and refacto texttest_fixture

--- texttest_fixture.py
+++ texttest_fixture.py
@@ -1,30 +1,39 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from gilded_rose import *
+from gilded_rose import Item, GildedRose
 
-if __name__ == "__main__":
-    print ("OMGHAI!")
+
+def main(days):
+    # Créez une liste d'objets
     items = [
-             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Item(name="Aged Brie", sell_in=2, quality=0),
-             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-             Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
-            ]
+        Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        Item(name="Aged Brie", sell_in=2, quality=0),
+        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        Item(name="Conjured Mana Cake", sell_in=3, quality=6),
+    ]
 
-    days = 2
-    import sys
-    if len(sys.argv) > 1:
-        days = int(sys.argv[1]) + 1
+    # Mettez à jour la qualité des objets sur plusieurs jours
     for day in range(days):
-        print("-------- day %s --------" % day)
-        print("name, sellIn, quality")
+        print("-------- jour %s --------" % day)
+        print("nom, date de vente, qualité")
         for item in items:
             print(item)
         print("")
         GildedRose(items).update_quality()
+
+
+if __name__ == "__main__":
+    # Si un argument est fourni, utilisez-le comme nombre de jours à simuler
+    # Sinon, simulez seulement 2 jours
+    import sys
+    if len(sys.argv) > 1:
+        days = int(sys.argv[1])
+    else:
+        days = 2
+    main(days)
